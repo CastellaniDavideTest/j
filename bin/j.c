32d1fa674730b28d13e31357f10ea828fc7d52ae
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-29 11:15:59 +0100

    Created bin/j.c

---
+++ bin/j.c
@@ -0,0 +1,22 @@
+/**
+ * @file j.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief 
+ *
+ * @ingroup j
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("j");
+	return 0;
+}
